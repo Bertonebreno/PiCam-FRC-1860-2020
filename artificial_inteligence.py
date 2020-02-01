0e71ce38af3a32d8bf2bd1befd62ebdf44c31733
Pedro Cleto <cl3t0.sh@gmail.com>
2020-02-01 17:46:06 -0200

    changing AI model (now using Keras)

--- artificial_inteligence.py
+++ artificial_inteligence.py
@@ -5,19 +5,20 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD
 
+training = False
+
 w = 480
 h = 270
 
 model = Sequential()
-model.add(Dense(32, input_dim=3*w*h))
-model.add(Dense(16))
-model.add(Dense(3))
-sgd = SGD(lr=5e-11)
+model.add(Dense(3, input_dim=3*w*h))
+sgd = SGD(lr=1e-10)
 model.compile(loss='mse',
               optimizer=sgd,
               metrics=['accuracy'])
 
-model.load_weights('model_weights.h5')
+if training == False:
+    model.load_weights('model_weights.h5')
 
 def loading_training_dataset():
     images = []
@@ -48,26 +49,29 @@ def loading_training_dataset():
 
 cap = cv2.VideoCapture("http://131.173.8.23/mjpg/video.mjpg")
 
-# images, labels = loading_training_dataset()
-# model.fit(images, labels,
-#           epochs=100,
-#           batch_size=40)
+if training:
+    images, labels = loading_training_dataset()
+    model.fit(images, labels,
+            epochs=100,
+            batch_size=32,
+            shuffle=True)
 
-# model.save_weights('model_weights.h5')
+    model.save_weights('model_weights.h5')
 
-while True:
-    ret, frame = cap.read()
-    new_frame = cv2.resize(frame, (w, h))
-    X = np.reshape(new_frame, (3*w*h))
-    result = model.predict(np.array([X]))
-    x = abs(result[0][0])
-    y = abs(result[0][1])
-    r = abs(result[0][2])
-    print("x:",x)
-    print("y:",y)
-    print("r:",r)
-    cv2.circle(new_frame, (int(x), int(y)), int(r), (0, 0, 255), 4)
-    cv2.circle(new_frame, (int(x), int(y)), 2, (0, 0, 255), 4)
-    cv2.imshow("image", new_frame)
-    if cv2.waitKey(1) == 27:
-        exit(0)
+if training == False:
+    while True:
+        ret, frame = cap.read()
+        new_frame = cv2.resize(frame, (w, h))
+        X = np.reshape(new_frame, (3*w*h))
+        result = model.predict(np.array([X]))
+        x = abs(result[0][0])
+        y = abs(result[0][1])
+        r = abs(result[0][2])
+        print("x:",x)
+        print("y:",y)
+        print("r:",r)
+        cv2.circle(new_frame, (int(x), int(y)), int(r), (0, 0, 255), 4)
+        cv2.circle(new_frame, (int(x), int(y)), 2, (0, 0, 255), 4)
+        cv2.imshow("image", new_frame)
+        if cv2.waitKey(1) == 27:
+            exit(0)
